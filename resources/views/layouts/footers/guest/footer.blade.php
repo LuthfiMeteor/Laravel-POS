58e495b82a70179914a926dae5656d15d7d59c60
luthfiMeteor <xoneskyone@gmail.com>
2023-05-06 10:41:37 +0700

    Tambah Helpers Dan Struktur Database

--- resources/views/layouts/footers/guest/footer.blade.php
+++ resources/views/layouts/footers/guest/footer.blade.php
@@ -1,6 +1,6 @@
   <!-- -------- START FOOTER 3 w/ COMPANY DESCRIPTION WITH LINKS & SOCIAL ICONS & COPYRIGHT ------- -->
   <footer class="footer py-5">
-    <div class="container">
+  <div class="container">
       <div class="row">
       <div class="col-lg-8 mb-4 mx-auto text-center">
       </div>
